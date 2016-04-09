9af08d4f42517015a8ac451d3c71e726ace70507
Kevin Brebanov <kevin.brebanov@gmail.com>
2016-04-09 19:28:13 -0400

    Enable logging

--- nose-bleed.go
+++ nose-bleed.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"time"
 
@@ -22,6 +23,7 @@ func main() {
 	exchange := flag.String("exchange", "", "RabbitMQ exchange name")
 	filter := flag.String("filter", "", "Berkley Packet Filter (BPF)")
 	showVersion := flag.Bool("version", false, "Show version")
+	logFilePath := flag.String("log", "./nose-bleed.log", "Path to log file")
 
 	flag.Parse()
 
@@ -29,6 +31,15 @@ func main() {
 		fmt.Println(version)
 		os.Exit(0)
 	}
+
+	// Configure logging
+	logFile, err := os.OpenFile(*logFilePath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer logFile.Close()
+	log.SetOutput(logFile)
 
 	// Start sniffing
 	sniffer.Run(*device, int32(*snaplen), *promiscuous, *timeout,
